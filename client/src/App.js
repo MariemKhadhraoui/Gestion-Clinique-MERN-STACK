98b5ff922682e3958c43ed8f3814e123bdeac9bd
MariemKhadhraoui <mariem.khadhraoui@esprit.tn>
2023-09-10 13:03:53 +0200

    client v0.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import { Routes, Route } from "react-router-dom";
-import './App.css';
-import Clinique from "./components/Clinique";
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import React from 'react';
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+import { StyledChart } from './components/chart';
+import ScrollToTop from './components/scroll-to-top';
 
 
+// ----------------------------------------------------------------------
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-        <Routes>
-        <Route path="/clinique" element={<Clinique />}> </Route>
-         </Routes>
-    </div>
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <ScrollToTop />
+          <StyledChart />
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+    
   );
 }
-
-export default App;
